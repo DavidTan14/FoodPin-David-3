2704babe6c1a2a1f7be995d69b227f12c6078aa0
DavidTan14 <410621214@gms.ndhu.edu.tw>
2021-01-03 23:29:29 +0800

    Initial Commit

---
+++ FoodPin/Controller/MainPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainPageViewController.swift
+//  FoodPin
+//
+//  Created by 林哲豪 on 2021/1/3.
+//  Copyright © 2021 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
